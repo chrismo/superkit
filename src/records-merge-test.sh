dd12888721f3a3b25a8b356029c2dc88ac164ea9
chrismo <chrismo@clabs.org>
2025-01-16 23:11:54 -0600

    merge_records looking pretty decent
    It's still a funky string hack, vs. something iterative or
    straight-forward.

--- src/records-merge-test.sh
+++ src/records-merge-test.sh
@@ -2,6 +2,19 @@
 
 set -euo pipefail
 
-zq -I merge_records.spq '[{a:1},{b:2},{c:3},{d:4}] | merge_records()'
-zq -I merge_records.spq '[{a:1},{b:"sandwich",c:333}] | merge_records()'
-zq -I merge_records.spq '[{a:1},{b:{c:333}}] | merge_records()'
+this_dir=$(dirname "${BASH_SOURCE[0]}")
+
+_assert '{a:1,b:2,c:3,d:4}' \
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:2},{c:3},{d:4}] | merge_records()')"
+_assert '{a:1}' \
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{a:1}] | merge_records()')"
+_assert '{a:1,b:"sandwich",c:333}' \
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:"sandwich",c:333}] | merge_records()')"
+_assert '{a:1,b:{c:333}}' \
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:333}}] | merge_records()')"
+_assert '{a:1,b:{c:3,d:4},e:[5,{f:6}]}' \
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:3, d:4}},{e:[5, {f:6}]}] | merge_records()')"
+_assert '{a:1,b:{c:3,d:4},e:[{f:6,g:7}]}' \
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:3, d:4}},{e:[{f:6},{g:7}]}] | merge_records()')"
+_assert '{a:1,b:{c:{d:2}}}' \
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:{d:2}}}] | merge_records()')"
--- test.sh
+++ test.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-# how to do discovery of the tests?
+set -euo pipefail
+
+function _assert() {
+  local -r expected="$1"
+  local -r actual="$2"
+
+  if [ "$expected" != "$actual" ]; then
+    echo "test failed: expected <$expected> actual <$actual>"
+  else
+    echo -n '.'
+  fi
+}
+
+for test_fn in ./src/*-test.sh; do
+  # shellcheck disable=SC1090
+  source "${test_fn}"
+done
+
+echo
