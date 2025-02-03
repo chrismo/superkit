e68d6ec24e20a2d67d374ef1051c1c9661e3338c
chrismo <chrismo@clabs.org>
2025-02-02 18:57:06 -0600

    Some renaming and README thoughts

--- README.md
+++ README.md
@@ -16,3 +16,16 @@ side-by-side.
 
 The release process will produce a single combined file that will be installed
 into the home directory of the user executing the install.sh script.
+
+## Multiple Versions
+
+Right now `super` is pre-release and the team has a lot of work ahead of them,
+so `zq` is probably going to stay at 1.18 for a while. 
+
+Separate branches seems reasonable. Working on diffs between them might be a
+pain though? Maybe just research this. I could use git worktrees to help prolly.
+             
+## TODO
+
+[//]: # (TODO: _ALWAYS_ a k prefix? In case of future name collisions?) 
+[//]: # (TODO: Then always a k prefix on the spq files as well?) 
--- src/records-merge-test.sh
+++ src/records-merge-test.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 this_dir=$(dirname "${BASH_SOURCE[0]}")
 
 _assert '{a:1,b:2,c:3,d:4}' \
-  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:2},{c:3},{d:4}] | merge_records()')"
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:2},{c:3},{d:4}] | kmerge_records()')"
 _assert '{a:1}' \
-  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{a:1}] | merge_records()')"
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{a:1}] | kmerge_records()')"
 _assert '{a:1,b:"sandwich",c:333}' \
-  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:"sandwich",c:333}] | merge_records()')"
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:"sandwich",c:333}] | kmerge_records()')"
 _assert '{a:1,b:{c:333}}' \
-  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:333}}] | merge_records()')"
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:333}}] | kmerge_records()')"
 _assert '{a:1,b:{c:3,d:4},e:[5,{f:6}]}' \
-  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:3, d:4}},{e:[5, {f:6}]}] | merge_records()')"
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:3, d:4}},{e:[5, {f:6}]}] | kmerge_records()')"
 _assert '{a:1,b:{c:3,d:4},e:[{f:6,g:7}]}' \
-  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:3, d:4}},{e:[{f:6},{g:7}]}] | merge_records()')"
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:3, d:4}},{e:[{f:6},{g:7}]}] | kmerge_records()')"
 _assert '{a:1,b:{c:{d:2}}}' \
-  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:{d:2}}}] | merge_records()')"
+  "$(zq -z -I "$this_dir"/records.spq '[{a:1},{b:{c:{d:2}}}] | kmerge_records()')"
