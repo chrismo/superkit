d3fd2c50eb32d77f1c82c5207e7f259e7567d3ef
chrismo <chrismo@clabs.org>
2025-03-06 09:53:09 -0600

    spq-doc.sh: parse multiples from file!
    Not to shabby ...

--- spq-doc.sh
+++ spq-doc.sh
@@ -8,6 +8,9 @@ cat ./src/format.spq |
 super -i line -c '
   const patterns="SKDOC \\{skdoc:.*?\\}func \\w+"
 
-  collect(this)
-  | join(this, "")
-  | grok(".*?%{SKDOC:skdoc_records}", this, patterns)' -
+  collect(this)    // collect all lines to get the entire file
+  | join(this, "") // make it all one string
+  | split(this, "{skdoc")[1:] // drop the 0th item before the first occurrence
+  | over this
+  | f"\{skdoc{this}" // restore the split on bit
+  | grok(".*?%{SKDOC:skdoc_record}", this, patterns)' -
