b570eaa0dd64f7d307f7d4ecf04e776f159815eb
chrismo <chrismo@clabs.org>
2025-03-09 14:45:13 -0500

    util: more testing-in-docker helper scripts

--- util/launch_ubuntu.sh
+++ util/launch_ubuntu.sh
@@ -9,7 +9,6 @@ function _script_dir() {
 function default() {
 
   docker build -f "$(_script_dir)"/sk-ubuntu.dockerfile \
-    $install_bat $install_fzf $install_glow $install_super $install_zq \
     --progress plain \
     -t superkit .
 
@@ -17,48 +16,4 @@ function default() {
   docker run --name superkit -it superkit:latest /bin/bash
 }
 
-function _usage() {
-  cat <<EOF
--b  Install bat
--f  Install fzf
--g  Install glow
--s  Install super
--z  Install zq
-EOF
-}
-
-function usage() {
-  _usage | less -FX
-}
-
-declare install_bat
-declare install_glow
-declare install_fzf
-declare install_super
-declare install_zq
-
-if [ $# -eq 0 ]; then
-  default
-else
-  while getopts "hbfgsz" opt; do
-    case $opt in
-    h)
-      usage
-      exit 0
-      ;;
-    b) install_bat="--build-arg install_bat=true" ;;
-    f) install_fzf="--build-arg install_fzf=true" ;;
-    g) install_glow="--build-arg install_glow=true" ;;
-    s) install_super="--build-arg install_super=true" ;;
-    z) install_zq="--build-arg install_zq=true" ;;
-    \?) # ignore invalid options
-      ;;
-    esac
-  done
-
-  # Remove options processed by getopts, so the remaining args can be handled
-  # positionally.
-  shift $((OPTIND - 1))
-
-  default "$@"
-fi
+default
