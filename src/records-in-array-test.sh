edf7b1629c44ca067b2d6170e1f1f1cf34911f85
chrismo <chrismo@clabs.org>
2025-03-14 09:09:10 -0500

    re-org sk_in_array to array.spq
    And update the test infra to accept a comma-delimited list of include
    files to include.

--- src/records-in-array-test.sh
+++ src/records-in-array-test.sh
@@ -2,11 +2,9 @@
 
 set -euo pipefail
 
-this_dir=$(dirname "${BASH_SOURCE[0]}")
-
-zq_and_super "$this_dir"/records.spq '{a:1} | sk_in_array(this)' '[{a:1}]'
-zq_and_super "$this_dir"/records.spq '[{a:1}] | sk_in_array(this)' '[{a:1}]'
-zq_and_super "$this_dir"/records.spq '[[1]] | sk_in_array(this)' '[[1]]'
-zq_and_super "$this_dir"/records.spq 'yield 1 | sk_in_array(this)' '[1]'
-zq_and_super "$this_dir"/records.spq 'yield "a" | sk_in_array(this)' '["a"]'
-zq_and_super "$this_dir"/records.spq '|{"a":1}| | sk_in_array(this)' '[|{"a":1}|]'
+zq_and_super array.spq,records.spq '{a:1} | sk_in_array(this)' '[{a:1}]'
+zq_and_super array.spq,records.spq '[{a:1}] | sk_in_array(this)' '[{a:1}]'
+zq_and_super array.spq,records.spq '[[1]] | sk_in_array(this)' '[[1]]'
+zq_and_super array.spq,records.spq 'yield 1 | sk_in_array(this)' '[1]'
+zq_and_super array.spq,records.spq 'yield "a" | sk_in_array(this)' '["a"]'
+zq_and_super array.spq,records.spq '|{"a":1}| | sk_in_array(this)' '[|{"a":1}|]'
